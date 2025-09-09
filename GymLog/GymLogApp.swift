9744a44dad95bba9a5af0ba057c0146d1d20e707
ALI <alinasserkhazali@gmail.com>
2025-09-09 04:33:23 +0300

    Initial Commit

---
+++ GymLog/GymLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymLogApp.swift
+//  GymLog
+//
+//  Created by Ali Al-Khazali on 9/9/25.
+//
+
+import SwiftUI
+
+@main
+struct GymLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
